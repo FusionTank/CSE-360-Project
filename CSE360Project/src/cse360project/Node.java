b1d7d4682a9dd898c6fa31077b9e559eedcad6a2
Ben Hibler <benhibler@gmail.com>
2018-10-06 16:23:40 -0700

    Update Node.java

--- CSE360Project/src/cse360project/Node.java
+++ CSE360Project/src/cse360project/Node.java
@@ -1,1 +1,14 @@
+// Anibal, feel free to add your Node class here. It is convention to have the first letter
+// of a class capitilized. 
 
+// Once you copy your code in here scroll down and put in a short description title where 
+// it has ghosted " Update Node.java" in the textbox.
+
+// Then hit commit. Once you do this, it'll save over the existing file. However, this is
+// the power of git and Github. Here on Github it will have a VERSION history saved. This 
+// allows us to go back in time and see what has changed within an individual class. We will
+// be able to see every change this way. i.e. version control. 
+
+// If we mess up code we can simply look at the history and pull an old file.
+
+// :)
